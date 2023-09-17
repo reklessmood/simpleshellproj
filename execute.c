4602ceaf9a4f26658bd908fd7e4fb40ef5b91712
reklessmood <nizarbellamqaddam@gmail.com>
2023-09-17 23:26:46 +0200

    update

---
+++ execute.c
@@ -0,0 +1,35 @@
+#include "main.h"
+/**
+ * execute - Function to execute the arguments
+ * @args: The arguments parsed from the input line
+ *
+ * Return: The result of the corresponding function or launch function
+ */
+int execute(char **args)
+{
+char *built_in_str[] = {
+		"exit",
+		"env"
+	};
+int (*built_in_func[]) (char **) = {
+		&built_in_exit,
+		&built_in_env
+	};
+
+	int i;
+
+	if (args[0] == NULL) /* If there are no arguments */
+	{
+		return (1); /* Return 1 to continue the loop in main function */
+	}
+	/* Loop through the built-in commands array */
+	for (i = 0; i < num_built_ins(); i++)
+	{/* If the first argument matches a built-in command name */
+		if (strcmp(args[0], built_in_str[i]) == 0)
+		{/* Call the corresponding function and return its result */
+			return ((*built_in_func[i])(args));
+		}
+	}
+	/* If no built-in command matches, launch the program and return its result */
+	return (launch(args));
+}
